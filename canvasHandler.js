26128a2e7c8056a789b11afbe73673363661b304
Valentin Gimonnet <valentin.gimonnet18@gmail.com>
2021-09-23 10:43:51 +0200

    change style + correction game over

--- canvasHandler.js
+++ canvasHandler.js
@@ -103,7 +103,7 @@ function createBall(frequency) {
       const pourcentage = (score / maxScore) * 100;
       document.querySelector('#score').innerHTML = Math.ceil(pourcentage) + '%';
       if (pourcentage < 50 && maxScore > 20) {
-        sketch.stopMusic();
+        // sketch.stopMusic();
         document.querySelector('#myModal').style.display = 'block';
         document.querySelector('#game-over').style.display = 'block';
       }
